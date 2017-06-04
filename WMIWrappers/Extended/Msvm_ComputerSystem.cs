598a9017493275a534d24e511692aaf6dc5088d4
isaev-bolsen <al_mamistov@mail.ru>
2017-06-04 16:01:19 +0300

    Turning on

--- WMIWrappers/Extended/Msvm_ComputerSystem.cs
+++ WMIWrappers/Extended/Msvm_ComputerSystem.cs
@@ -9,9 +9,28 @@ namespace WMIWrappers.Extended
 {
     public class Msvm_ComputerSystem : Raw.Msvm_ComputerSystem
     {
+        private const string RequestStateChange = "RequestStateChange";
+
         public Msvm_ComputerSystem(ManagementObject instance) : base(instance)
         {
 
+        }
+
+        private void SetState(uint state)
+        {
+            ManagementBaseObject pms = Instance.GetMethodParameters(RequestStateChange);
+            pms["RequestedState"] = state;
+            Instance.InvokeMethod(RequestStateChange, pms, null);
+        }
+
+        public void TurnOn()
+        {
+            SetState(2);
+        }
+
+        public void TurnOff()
+        {
+            SetState(3);
         }
     }
 }
