889815383ee6914289909f6378667501e51caf41
isaev-bolsen <al_mamistov@mail.ru>
2017-06-05 21:27:52 +0300

    Start to Add GetMsvm_VirtualSystemSettingData

--- WMIWrappers/Extended/Msvm_ComputerSystem.cs
+++ WMIWrappers/Extended/Msvm_ComputerSystem.cs
@@ -1,9 +1,11 @@
 ﻿using System.Management;
+using System.Linq;
 
 namespace WMIWrappers.Extended
 {
     public class Msvm_ComputerSystem : Raw.Msvm_ComputerSystem
     {
+        private const string VSSettingsData = "Msvm_VirtualSystemSettingData";
         private const string RequestStateChange = "RequestStateChange";
 
         public Msvm_ComputerSystem(ManagementObject instance) : base(instance)
@@ -16,6 +18,13 @@ namespace WMIWrappers.Extended
             ManagementBaseObject pms = Instance.GetMethodParameters(RequestStateChange);
             pms["RequestedState"] = state;
             Instance.InvokeMethod(RequestStateChange, pms, null);
+        }
+
+        private ManagementObject GetMsvm_VirtualSystemSettingData(string vmName)
+        {
+            ManagementObjectCollection res = new WMIHelper(Instance.Scope).GetByClassName(VSSettingsData);
+
+            return res.OfType<ManagementObject>().Single();
         }
 
         public void Put()
