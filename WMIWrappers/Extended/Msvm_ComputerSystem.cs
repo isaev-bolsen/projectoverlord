181df47ad4930110236ef13a9c8b70a9a7dd2d62
isaev-bolsen <al_mamistov@mail.ru>
2017-06-07 20:23:25 +0300

    Msvm_ComputerSystem : cleanup

--- WMIWrappers/Extended/Msvm_ComputerSystem.cs
+++ WMIWrappers/Extended/Msvm_ComputerSystem.cs
@@ -9,11 +9,12 @@ namespace WMIWrappers.Extended
         private const string RequestStateChange = "RequestStateChange";
         private readonly Msvm_VirtualSystemSettingData Msvm_VirtualSystemSettingData;
 
-        public Msvm_ComputerSystem(ManagementObject instance) : base(instance)
+        public Msvm_ComputerSystem(ManagementObject instance) : base(instance) { }
+
+        public Msvm_VirtualSystemSettingData GetMsvm_VirtualSystemSettingData()
         {
-            Msvm_VirtualSystemSettingData = GetMsvm_VirtualSystemSettingData();
-            Msvm_VirtualSystemSettingData.ElementName = "SLAVA"; //need to run specific command
-            Msvm_VirtualSystemSettingData.Put();
+            return new WMIHelper(Instance.Scope).GetByClassName(VSSettingsData).OfType<ManagementObject>().
+                Select(o => new Msvm_VirtualSystemSettingData(o)).Single(s => s.VirtualSystemIdentifier == Name);
         }
 
         private void SetState(uint state)
@@ -21,14 +22,6 @@ namespace WMIWrappers.Extended
             ManagementBaseObject pms = Instance.GetMethodParameters(RequestStateChange);
             pms["RequestedState"] = state;
             Instance.InvokeMethod(RequestStateChange, pms, null);
-        }
-
-        public Msvm_VirtualSystemSettingData GetMsvm_VirtualSystemSettingData()
-        {
-            Msvm_VirtualSystemSettingData[] results = new WMIHelper(Instance.Scope).GetByClassName(VSSettingsData).
-                OfType<ManagementObject>().Select(o => new Msvm_VirtualSystemSettingData(o)).ToArray(); ;
-
-            return results.Single(s => s.VirtualSystemIdentifier == Name);
         }
 
         public void TurnOn()
