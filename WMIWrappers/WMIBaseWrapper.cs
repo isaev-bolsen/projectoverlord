834b58ecbef04270d8ad5b05747755bff765632a
isaev-bolsen <al_mamistov@mail.ru>
2017-06-08 20:43:59 +0300

    Basic WMIWrapper

---
+++ WMIWrappers/WMIBaseWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Management;
+
+namespace WMIWrappers
+{
+    public abstract class WMIWrapper
+    {
+        private ManagementObject _instance;
+
+        protected ManagementObject Instance => _instance;
+        protected ManagementScope Scope => Instance.Scope;
+
+        protected WMIWrapper(ManagementObject instance)
+        {
+            _instance = instance;
+        }
+
+        public virtual void Refresh()
+        {
+            _instance = new ManagementObject(Instance.Path);
+        }
+    }
+}
