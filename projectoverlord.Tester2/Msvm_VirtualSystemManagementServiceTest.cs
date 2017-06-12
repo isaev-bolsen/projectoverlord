8aeeb6aa04cb0d81a00271a8a39a1e450e8ff781
isaev-bolsen <al_mamistov@mail.ru>
2017-06-12 14:57:39 +0300

    Replace tester

---
+++ projectoverlord.Tester2/Msvm_VirtualSystemManagementServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WMIWrappers.Extended;
+
+namespace projectoverlord.Tester2
+{
+    [TestClass]
+    public class Msvm_VirtualSystemManagementServiceTest
+    {
+        const string VMName = "EXAMPLE";
+
+        [TestMethod]
+        public void CreateWithDefault()
+        {
+            Msvm_VirtualSystemManagementService VSMService = new Msvm_VirtualSystemManagementService(Environment.MachineName);
+            IEnumerable<Msvm_ComputerSystem> ExistingVMs = VSMService.GetVMByDispalyName(VMName);
+            Msvm_ComputerSystem newOne = VSMService.CreateVm(VMName);
+            IEnumerable<Msvm_ComputerSystem> After = VSMService.GetVMByDispalyName(VMName);
+
+            Assert.AreEqual(ExistingVMs.Count() + 1, After.Count());
+        }
+    }
+}
---
+++ projectoverlord.Tester2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("projectoverlord.Tester2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("projectoverlord.Tester2")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ddb73c78-71bf-44db-a26d-373cf1b8dce8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
